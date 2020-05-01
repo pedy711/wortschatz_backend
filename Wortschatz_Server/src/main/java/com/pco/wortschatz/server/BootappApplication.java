41d5f6b856d0ebad17376d349d35d89cc8ca3cc2
pedy711 <pedy711@users.noreply.github.com>
2020-05-01 21:14:36 +0200

    Migrating from Maven to Gradle

--- Wortschatz_Server/src/main/java/com/pco/wortschatz/server/BootappApplication.java
+++ Wortschatz_Server/src/main/java/com/pco/wortschatz/server/BootappApplication.java
@@ -1,20 +1,14 @@
 package com.pco.wortschatz.server;
 
-import java.util.ArrayList;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.stereotype.Component;
 
-//import com.pco.hamrah.common.model.User;
-//import com.pco.hamrah.service.UserService;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.pco.wortschatz")
+@EnableSwagger2
 public class BootappApplication {
 
 	public static void main(String[] args) {
